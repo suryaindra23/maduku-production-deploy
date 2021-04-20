250e3bddcdb88bdd1cfc508bbf3b774e9febbf97
Fat Choi <suryaindra019@gmail.com>
2021-04-20 14:40:54 +0700

    main

--- src/Components/Layouts/HoneyCard/HoneyCard.jsx
+++ src/Components/Layouts/HoneyCard/HoneyCard.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-import RenderImage from "../../../Commons/ReactSmoothImage"
-import "../../../Assets/dist/honeycard.scss"
+import RenderImage from "../../../Commons/ReactSmoothImage";
+import "../../../Assets/dist/honeycard.scss";
 
 export default function HoneyCard({
   src,
@@ -13,44 +13,67 @@ export default function HoneyCard({
   taste,
   color,
   priceLists,
+  listdics,
 }) {
-  const [imgSrc, setSrc] = useState(src)
-  const [active, setActive] = useState(1)
-  const [price, setPrice] = useState("")
+  const [imgSrc, setSrc] = useState(src);
+  const [active, setActive] = useState(1);
+  const [active1, setActive1] = useState(1);
+  const [price, setPrice] = useState("");
+  const [pricedis, setPricedis] = useState("");
   const borderRadius =
-    window.innerWidth < 768 ? "20px 20px 0 0" : "20px 0 0 20px"
-  const height = window.innerWidth < 768 ? "250px" : "450px"
-  const width = window.innerWidth < 768 ? "100%" : "400px"
+    window.innerWidth < 768 ? "20px 20px 0 0" : "20px 0 0 20px";
+  const height = window.innerWidth < 768 ? "250px" : "450px";
+  const width = window.innerWidth < 768 ? "100%" : "400px";
   const style = {
     borderRadius,
     height,
     overflow: "hidden",
     width,
-  }
+  };
 
   useEffect(() => {
     switch (active) {
       case 1:
-        setPrice(priceLists.price125ml)
-        break
+        setPrice(priceLists.price125ml);
+        break;
 
       case 2:
-        setPrice(priceLists.price325ml)
-        break
+        setPrice(priceLists.price325ml);
+        break;
       case 3:
-        setPrice(priceLists.price500ml)
-        break
+        setPrice(priceLists.price500ml);
+        break;
       case 4:
-        setPrice(priceLists.price1000ml)
-        break
+        setPrice(priceLists.price1000ml);
+        break;
       default:
-        break
+        break;
     }
-  }, [active])
+  }, [active]);
 
-  const onClick = (number, source) => {
-    setActive(number)
-    setSrc(source)
+  useEffect(() => {
+    switch (active1) {
+      case 1:
+        setPricedis(listdics.pricedis125ml);
+        break;
+      case 2:
+        setPricedis(listdics.pricedis325ml);
+        break;
+      case 3:
+        setPricedis(listdics.pricedis500ml);
+        break;
+      case 4:
+        setPricedis(listdics.pricedis1000ml);
+        break;
+      default:
+        break;
+    }
+  }, [active1]);
+
+  function onClick(number1, number, source) {
+    setActive1(number1);
+    setActive(number);
+    setSrc(source);
   }
 
   return (
@@ -62,25 +85,34 @@ export default function HoneyCard({
         <div className="HoneyCardContent">
           <h3>{title}</h3>
           <p>{desc}</p>
-          <p>Rasa &emsp;: {taste}</p>
+          <p>Rasa &nbsp;&nbsp;: {taste}</p>
           <p>Warna : {color}</p>
           <p>
-            Harga &nbsp;: <strong className="price">{price}</strong>
+            Harga Normal &nbsp;: <br></br>
+            <strong className="Pr">
+              <s>{pricedis}</s>
+            </strong>
+          </p>
+          <p>
+            Harga Discount &nbsp;:&nbsp;
+            <strong className="price">
+              <h2>{price}</h2>
+            </strong>
           </p>
           <h3>VARIAN UKURAN</h3>
           <div className="row">
             <div className="col-6">
               <button
-                className={active === 1 ? "active" : ""}
-                onClick={() => onClick(1, src)}
+                className={active1 === 1 || active === 1 ? "active" : ""}
+                onClick={() => onClick(1, 1, src)}
               >
                 125 mL
               </button>
             </div>
             <div className="col-6">
               <button
-                className={active === 2 ? "active" : ""}
-                onClick={() => onClick(2, src2)}
+                className={active1 === 2 || active === 2 ? "active" : ""}
+                onClick={() => onClick(2, 2, src2)}
               >
                 325 mL
               </button>
@@ -89,16 +121,16 @@ export default function HoneyCard({
           <div className="row">
             <div className="col-6">
               <button
-                className={active === 3 ? "active" : ""}
-                onClick={() => onClick(3, src3)}
+                className={active1 === 3 || active === 3 ? "active" : ""}
+                onClick={() => onClick(3, 3, src3)}
               >
                 500 mL
               </button>
             </div>
             <div className="col-6">
               <button
-                className={active === 4 ? "active" : ""}
-                onClick={() => onClick(4, src4)}
+                className={active1 === 4 || active === 4 ? "active" : ""}
+                onClick={() => onClick(4, 4, src4)}
               >
                 1000 mL
               </button>
@@ -107,5 +139,5 @@ export default function HoneyCard({
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/Components/Pages/PureHoney/PureHoney.jsx
+++ src/Components/Pages/PureHoney/PureHoney.jsx
@@ -1,56 +1,65 @@
-import React from "react"
+import React from "react";
 
-import HoneyCard from "../../Layouts/HoneyCard/HoneyCard"
-import multiflora from "../../../Assets/img/purehoney/Multiflora.jpg"
-import durian from "../../../Assets/img/purehoney/Durian.jpg"
-import randu from "../../../Assets/img/purehoney/Randu.jpg"
-import kelengkeng from "../../../Assets/img/purehoney/Kelengkeng.jpg"
-import kopi from "../../../Assets/img/purehoney/Kopi.jpg"
-import kaliandra from "../../../Assets/img/purehoney/Kaliandra.jpg"
-import rambutan from "../../../Assets/img/purehoney/Rambutan.jpg"
-import mangga from "../../../Assets/img/purehoney/Mangga.jpg"
+import HoneyCard from "../../Layouts/HoneyCard/HoneyCard";
+import multiflora from "../../../Assets/img/purehoney/Multiflora.jpg";
+import durian from "../../../Assets/img/purehoney/Durian.jpg";
+import randu from "../../../Assets/img/purehoney/Randu.jpg";
+import kelengkeng from "../../../Assets/img/purehoney/Kelengkeng.jpg";
+import kopi from "../../../Assets/img/purehoney/Kopi.jpg";
+import kaliandra from "../../../Assets/img/purehoney/Kaliandra.jpg";
+import rambutan from "../../../Assets/img/purehoney/Rambutan.jpg";
+import mangga from "../../../Assets/img/purehoney/Mangga.jpg";
 
 //325 mL variant
-import multiflora2 from "../../../Assets/img/purehoney/varian2/Multiflora.jpg"
-import durian2 from "../../../Assets/img/purehoney/varian2/Durian.jpg"
-import randu2 from "../../../Assets/img/purehoney/varian2/Randu.jpg"
-import kelengkeng2 from "../../../Assets/img/purehoney/varian2/Kelengkeng.jpg"
-import kopi2 from "../../../Assets/img/purehoney/varian2/Kopi.jpg"
-import kaliandra2 from "../../../Assets/img/purehoney/varian2/Kaliandra.jpg"
-import rambutan2 from "../../../Assets/img/purehoney/varian2/Rambutan.jpg"
+import multiflora2 from "../../../Assets/img/purehoney/varian2/Multiflora.jpg";
+import durian2 from "../../../Assets/img/purehoney/varian2/Durian.jpg";
+import randu2 from "../../../Assets/img/purehoney/varian2/Randu.jpg";
+import kelengkeng2 from "../../../Assets/img/purehoney/varian2/Kelengkeng.jpg";
+import kopi2 from "../../../Assets/img/purehoney/varian2/Kopi.jpg";
+import kaliandra2 from "../../../Assets/img/purehoney/varian2/Kaliandra.jpg";
+import rambutan2 from "../../../Assets/img/purehoney/varian2/Rambutan.jpg";
+import mangga2 from "../../../Assets/img/purehoney/Mangga.jpg";
 
 //500 mL variant
-import multiflora3 from "../../../Assets/img/purehoney/varian3/Multiflora.jpg"
-import durian3 from "../../../Assets/img/purehoney/varian3/Durian.jpg"
-import randu3 from "../../../Assets/img/purehoney/varian3/Randu.jpg"
-import kelengkeng3 from "../../../Assets/img/purehoney/varian3/Kelengkeng.jpg"
-import kopi3 from "../../../Assets/img/purehoney/varian3/Kopi.jpg"
-import kaliandra3 from "../../../Assets/img/purehoney/varian3/Kaliandra.jpg"
-import rambutan3 from "../../../Assets/img/purehoney/varian3/Rambutan.jpg"
+import multiflora3 from "../../../Assets/img/purehoney/varian3/Multiflora.jpg";
+import durian3 from "../../../Assets/img/purehoney/varian3/Durian.jpg";
+import randu3 from "../../../Assets/img/purehoney/varian3/Randu.jpg";
+import kelengkeng3 from "../../../Assets/img/purehoney/varian3/Kelengkeng.jpg";
+import kopi3 from "../../../Assets/img/purehoney/varian3/Kopi.jpg";
+import kaliandra3 from "../../../Assets/img/purehoney/varian3/Kaliandra.jpg";
+import rambutan3 from "../../../Assets/img/purehoney/varian3/Rambutan.jpg";
+import mangga3 from "../../../Assets/img/purehoney/varian3/Mangga.jpg";
 
 //1000 mL variant
-import multiflora4 from "../../../Assets/img/purehoney/varian4/Multiflora.jpg"
-import durian4 from "../../../Assets/img/purehoney/varian4/Durian.jpg"
-import randu4 from "../../../Assets/img/purehoney/varian4/Randu.jpg"
-import kelengkeng4 from "../../../Assets/img/purehoney/varian4/Kelengkeng.jpg"
-import kopi4 from "../../../Assets/img/purehoney/varian4/Kopi.jpg"
-import kaliandra4 from "../../../Assets/img/purehoney/varian4/Kaliandra.jpg"
-import rambutan4 from "../../../Assets/img/purehoney/varian4/Rambutan.jpg"
+import multiflora4 from "../../../Assets/img/purehoney/varian4/Multiflora.jpg";
+import durian4 from "../../../Assets/img/purehoney/varian4/Durian.jpg";
+import randu4 from "../../../Assets/img/purehoney/varian4/Randu.jpg";
+import kelengkeng4 from "../../../Assets/img/purehoney/varian4/Kelengkeng.jpg";
+import kopi4 from "../../../Assets/img/purehoney/varian4/Kopi.jpg";
+import kaliandra4 from "../../../Assets/img/purehoney/varian4/Kaliandra.jpg";
+import rambutan4 from "../../../Assets/img/purehoney/varian4/Rambutan.jpg";
+import mangga4 from "../../../Assets/img/purehoney/varian4/Mangga.jpg";
 
-import "../../../Assets/dist/purehoney.scss"
+import "../../../Assets/dist/purehoney.scss";
 
 export default function PureHoney() {
   const products = [
     {
       title: "Multifora",
-      desc: "Madu dari berbagai jenis nectar bunga",
+      desc: "Madu dari berbagai jenis nectar bunga.",
       taste: "Manis aja",
       color: "Kuning emas",
       pricelists: {
-        price125ml: "Rp. 22.000",
-        price325ml: "Rp. 50.000",
-        price500ml: "Rp. 70.000",
-        price1000ml: "Rp. 95.000",
+        price125ml: "Rp. 24.000",
+        price325ml: "Rp. 54.000",
+        price500ml: "Rp. 73.500",
+        price1000ml: "Rp. 99.000",
+      },
+      listdics: {
+        pricedis125ml: "Rp. 40.000",
+        pricedis325ml: "Rp. 75.000",
+        pricedis500ml: "Rp. 110.000",
+        pricedis1000ml: "Rp. 170.000",
       },
       img: multiflora,
       img2: multiflora2,
@@ -59,14 +68,20 @@ export default function PureHoney() {
     },
     {
       title: "Durian",
-      desc: "Madu dari nectar bunga durian",
+      desc: "Madu dari nectar bunga durian.",
       taste: "Manis legit",
       color: "Coklat tua",
       pricelists: {
-        price125ml: "Rp. 25.000",
-        price325ml: "Rp. 52.000",
-        price500ml: "Rp. 82.000",
-        price1000ml: "Rp. 110.000",
+        price125ml: "Rp. 27.000",
+        price325ml: "Rp. 54.000",
+        price500ml: "Rp. 85.000",
+        price1000ml: "Rp. 114.000",
+      },
+      listdics: {
+        pricedis125ml: "Rp. 45.000",
+        pricedis325ml: "Rp. 80.000",
+        pricedis500ml: "Rp. 120.000",
+        pricedis1000ml: "Rp. 180.000",
       },
       img: durian,
       img2: durian2,
@@ -75,14 +90,20 @@ export default function PureHoney() {
     },
     {
       title: "Randu",
-      desc: "Madu dari nectar kapuk randu",
+      desc: "Madu dari nectar kapuk randu.",
       taste: "Manis asam",
       color: "Coklat bening",
       pricelists: {
-        price125ml: "Rp. 24.000",
-        price325ml: "Rp. 55.000",
-        price500ml: "Rp. 77.000",
-        price1000ml: "Rp. 105.000",
+        price125ml: "Rp. 26.000",
+        price325ml: "Rp. 58.500",
+        price500ml: "Rp. 80.500",
+        price1000ml: "Rp. 108.000",
+      },
+      listdics: {
+        pricedis125ml: "Rp. 42.000",
+        pricedis325ml: "Rp. 84.000",
+        pricedis500ml: "Rp. 114.000",
+        pricedis1000ml: "Rp. 175.000",
       },
       img: randu,
       img2: randu2,
@@ -91,14 +112,20 @@ export default function PureHoney() {
     },
     {
       title: "Kelengkeng",
-      desc: "Madu dari nectar bunga kelengkeng",
+      desc: "Madu dari nectar bunga kelengkeng.",
       taste: "Manis wangi",
       color: "Coklat kuning",
       pricelists: {
-        price125ml: "Rp. 28.000",
-        price325ml: "Rp. 62.000",
-        price500ml: "Rp. 85.000",
-        price1000ml: "Rp. 120.000",
+        price125ml: "Rp. 30.000",
+        price325ml: "Rp. 67.500",
+        price500ml: "Rp. 90.000",
+        price1000ml: "Rp. 124.000",
+      },
+      listdics: {
+        pricedis125ml: "Rp. 55.000",
+        pricedis325ml: "Rp. 90.000",
+        pricedis500ml: "Rp. 125.000",
+        pricedis1000ml: "Rp. 180.000",
       },
       img: kelengkeng,
       img2: kelengkeng2,
@@ -107,14 +134,20 @@ export default function PureHoney() {
     },
     {
       title: "Kopi",
-      desc: "Madu dari nectar bunga kopi",
+      desc: "Madu dari nectar bunga kopi.",
       taste: "Manis segar",
       color: "Coklat",
       pricelists: {
-        price125ml: "Rp. 26.000",
-        price325ml: "Rp. 52.000",
-        price500ml: "Rp. 85.000",
-        price1000ml: "Rp. 115.000",
+        price125ml: "Rp. 28.000",
+        price325ml: "Rp. 54.000",
+        price500ml: "Rp. 88.000",
+        price1000ml: "Rp. 118.000",
+      },
+      listdics: {
+        pricedis125ml: "Rp. 45.000",
+        pricedis325ml: "Rp. 86.000",
+        pricedis500ml: "Rp. 128.000",
+        pricedis1000ml: "Rp. 183.000",
       },
       img: kopi,
       img2: kopi2,
@@ -123,14 +156,20 @@ export default function PureHoney() {
     },
     {
       title: "Kaliandra",
-      desc: "Madu dari nectar bunga kaliandra",
+      desc: "Madu dari nectar bunga kaliandra.",
       taste: "Manis disukai anak",
       color: "Kuning telor",
       pricelists: {
-        price125ml: "Rp. 30.000",
-        price325ml: "Rp. 65.000",
-        price500ml: "Rp. 90.000",
-        price1000ml: "Rp. 120.000",
+        price125ml: "Rp. 32.000",
+        price325ml: "Rp. 72.500",
+        price500ml: "Rp. 95.000",
+        price1000ml: "Rp. 124.000",
+      },
+      listdics: {
+        pricedis125ml: "Rp. 60.000",
+        pricedis325ml: "Rp. 95.000",
+        pricedis500ml: "Rp. 130.000",
+        pricedis1000ml: "Rp. 190.000",
       },
       img: kaliandra,
       img2: kaliandra2,
@@ -139,14 +178,20 @@ export default function PureHoney() {
     },
     {
       title: "Rambutan",
-      desc: "Madu dari nectar bunga rambutan",
+      desc: "Madu dari nectar bunga rambutan.",
       taste: "Manis bikin nagih",
       color: "Coklat bening",
       pricelists: {
-        price125ml: "Rp. 28.000",
-        price325ml: "Rp. 55.000",
-        price500ml: "Rp. 85.000",
-        price1000ml: "Rp. 115.000",
+        price125ml: "Rp. 30.000",
+        price325ml: "Rp. 60.000",
+        price500ml: "Rp. 90.000",
+        price1000ml: "Rp. 118.000",
+      },
+      listdics: {
+        pricedis125ml: "Rp. 55.000",
+        pricedis325ml: "Rp. 90.000",
+        pricedis500ml: "Rp. 125.000",
+        pricedis1000ml: "Rp. 180.000",
       },
       img: rambutan,
       img2: rambutan2,
@@ -155,21 +200,27 @@ export default function PureHoney() {
     },
     {
       title: "Mangga",
-      desc: "Madu dari nectar bunga mangga",
+      desc: "Madu dari nectar bunga mangga.",
       taste: "Manis dan asam",
       color: "Coklat kuning",
       pricelists: {
-        price125ml: "Rp. 30.000",
-        price325ml: "Rp. 65.000",
-        price500ml: "Rp. 90.000",
+        price125ml: "Rp. 32.000",
+        price325ml: "Rp. 72.500",
+        price500ml: "Rp. 95.000",
         price1000ml: "Rp. 124.000",
       },
+      listdics: {
+        pricedis125ml: "Rp. 60.000",
+        pricedis325ml: "Rp. 95.000",
+        pricedis500ml: "Rp. 130.000",
+        pricedis1000ml: "Rp. 190.000",
+      },
       img: mangga,
-      img2: mangga,
-      img3: mangga,
-      img4: mangga,
+      img2: mangga2,
+      img3: mangga3,
+      img4: mangga4,
     },
-  ]
+  ];
 
   return (
     <div className="VariantContainer">
@@ -193,11 +244,12 @@ export default function PureHoney() {
                 taste={p.taste}
                 color={p.color}
                 priceLists={p.pricelists}
+                listdics={p.listdics}
               />
-            )
+            );
           })}
         </div>
       </div>
     </div>
-  )
+  );
 }
